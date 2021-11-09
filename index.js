d70e45e3767edc1cfef6a980904b45528be89a4c
Bruno Mendes <mendessbruno@gmail.com>
2021-11-09 19:23:50 -0300

    feat: completadas as rotas no index

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const controller = require('./controller');
+
 const app = express();
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.delete('/ads/:id', );
+app.delete('/ads/:id', controller.deleteById);
 
 app.get('/', );
-app.get('/ads', );
+app.get('/ads', controller.getAll);
 
-app.put('/ads/:id', );
+app.put('/ads/:id', controller.updateById);
 
-app.post('/ads', );
+app.post('/ads', controller.create);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
